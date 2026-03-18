4caad35dcf14e0a78da4c66b57f5f66efdca39f9
aur0ra <codeberg+aurora@schink.xyz>
2026-03-18 16:53:46 +0100

    fix: config file location

--- config/config.go
+++ config/config.go
@@ -1,6 +1,9 @@
 package config
 
 import (
+	"os"
+	"path/filepath"
+
 	"github.com/adrg/xdg"
 	"github.com/spf13/viper"
 )
@@ -13,17 +16,18 @@ func Init(cfgFile string) {
 		viper.SetConfigFile(cfgFile)
 	} else {
 		// Search config in home directory with name ".cage" (without extension).
-		viper.AddConfigPath(xdg.ConfigHome)
+		configPath := filepath.Join(xdg.ConfigHome, "cage")
+		os.MkdirAll(configPath, 0750)
+		viper.AddConfigPath(configPath)
 		viper.SetConfigType("yaml")
-		viper.SetConfigName(".cage")
+		viper.SetConfigName("config")
 	}
 
 	viper.AutomaticEnv() // read in environment variables that match
+	viper.SafeWriteConfig()
 
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err != nil {
 		panic(err)
 	}
-
-	viper.SafeWriteConfig()
 }
